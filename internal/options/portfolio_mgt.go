02863437217e6984f364134999210c99aa083859
theghostmac <theghostmac@gmail.com>
2023-11-29 05:32:05 +0100

    creating a backtesting environment for testing the setup i have already

--- internal/options/portfolio_mgt.go
+++ internal/options/portfolio_mgt.go
@@ -1,6 +1,10 @@
 package options
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // NewPortfolio creates a new portfolio with an initial cash balance.
 func NewPortfolio(initialCashBalance float64) *Portfolio {
@@ -23,5 +27,21 @@ func (p *Portfolio) PrintStatus() {
 // GetOptionBySymbol returns the Option object for a given symbol.
 func GetOptionBySymbol(symbol string) (Option, error) {
 	// TODO: implement this function to fetch Options from a data broker online.
+
+	// Fetch options details from and external data source.
+	// make HTTP requests and parse the response.
+
+	resp, err := http.Get("")
+	if err != nil {
+		return Option{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var option Option
+	if err := json.NewDecoder(resp.Body).Decode(&option); err != nil {
+		return Option{}, err
+	}
+
 	return Option{}, nil
 }
